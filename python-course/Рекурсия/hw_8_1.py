924d19b630ae9265ba819470c27d339d30a7b8ed
Vilorf <134338080+Vilorf@users.noreply.github.com>
2023-11-08 01:06:15 +0300

    Add files via upload

--- python-course/Рекурсия/hw_8_1.py
+++ python-course/Рекурсия/hw_8_1.py
@@ -1,1 +1,19 @@
+list1 = [81, 15, 18, 46, 64, 85, 74, 8, 20]
+list2 = [14, 5, 83, 99, 11, 66, 98, 30, 58, 77, 99, 82, 75, 63]
 
+def getMax(user_list):
+    if len(user_list) == 1:
+        return user_list[0]
+
+    mx = user_list.pop()
+    cur = user_list.pop()
+
+    if cur > mx:
+        user_list.append(cur)
+        return getMax(user_list)
+    else:
+        user_list.append(mx)
+        return getMax(user_list)
+
+print(getMax(list1))
+print(getMax(list2))
